b55e2583a66576b4ee0fa2832c5327e90dd537d2
luchetto93 <guerrini_luc93@hotmail.com>
2017-05-30 08:13:19 +0200

    First_Commit

--- src/finaltermp2p/FinalTermP2P.java
+++ src/finaltermp2p/FinalTermP2P.java
@@ -8,6 +8,7 @@ import it.unimi.dsi.webgraph.ImmutableGraph;
 import it.unimi.dsi.webgraph.NodeIterator;
 import it.unimi.dsi.webgraph.algo.ConnectedComponents;
 import it.unimi.dsi.big.webgraph.algo.StronglyConnectedComponents;
+import it.unimi.dsi.webgraph.BVGraph;
 /**
  *
  * @author luca
@@ -20,6 +21,14 @@ public class FinalTermP2P {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("PUSH");
+        try{
+         int out_d;
+         
+         BVGraph graph = BVGraph.load("/home/luca/NetBeansProjects/JavaApplication11/bvtime0");
+         ComputeStatistics stat = new ComputeStatistics(graph);
+         stat.number_of_mutual_connections();
+        }catch(Exception e)
+        {}
     }
     
 }
